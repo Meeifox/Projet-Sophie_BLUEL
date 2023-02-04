7730de2c50e1902f9ced25c8906dc0a48d5a3797
Meeifox <nay.portier@gmail.Com>
2023-02-04 13:52:16 +0100

    Update 04-02-2023 - Add gallery filter by categories
    Adding LocalStorage function to not call backend each click

--- FrontEnd/scripts/index.js
+++ FrontEnd/scripts/index.js
@@ -1,10 +1,31 @@
 function docIsReady() {
-    const works = getAllWorks();
-    works.then(value => constructWorksItems(value));
-    
-    const categories = getAllCategories();
-    categories.then(value => filtersButton(value));
-    
+    let worksArray = [];
+    let categoriesArray = [];
+
+    // Check if the worksArray and categoriesArray exist in the local storage
+    if (localStorage.getItem(`worksArray`) && localStorage.getItem(`categoriesArray`)) {
+        worksArray = JSON.parse(localStorage.getItem(`worksArray`));
+        categoriesArray = JSON.parse(localStorage.getItem(`categoriesArray`));
+
+        // Construct works items and filter buttons
+        constructWorksItems(worksArray);
+        filtersButton(categoriesArray);
+
+    } else {
+        // Call API to get all works
+        getAllWorks().then(value => {
+            worksArray = value;
+            localStorage.setItem(`worksArray`, JSON.stringify(worksArray));
+            constructWorksItems(worksArray);
+        });
+
+        // Call API to get all categories
+        getAllCategories().then(value => {
+            categoriesArray = value;
+            localStorage.setItem(`categoriesArray`, JSON.stringify(categoriesArray));
+            filtersButton(categoriesArray);
+        });
+    }
 }
 
 /**
@@ -12,7 +33,7 @@ function docIsReady() {
 * @returns promise of works in array
 */
 async function getAllWorks() {
-    const works = await fetch("http://localhost:5678/api/works").then(d => d.json());
+    const works = await fetch(`http://localhost:5678/api/works`).then(d => d.json());
     return works;
 }
 
@@ -24,62 +45,84 @@ async function getAllWorks() {
 *     <figcaption>Abajour Tahina</figcaption>
 * </figure>
 * @param {an array full of Works items} worksArray 
+* @param
 */
-function constructWorksItems(worksArray) {
+function constructWorksItems(worksArray, selectedCategory) {
+    if (!selectedCategory) {
+        selectedCategory = 0;
+    }
     // get the Galery HTML, where i will add all works
-    const myGalleryHTML = document.getElementById("portfolio").getElementsByClassName("gallery")[0];
+    const myGalleryHTML = document.querySelector(`#portfolio > .gallery`);
+    myGalleryHTML.innerHTML = ``;
     // Iterate on works array
     for (const work of worksArray) {
-        console.log(work);
-        const figure = document.createElement("figure");  
-        
-        //Create the img element
-        const imageGallery = document.createElement("img");
-        imageGallery.setAttribute("alt", work.title);
-        imageGallery.setAttribute("src", work.imageUrl);
-        imageGallery.setAttribute("crossOrigin", "Anonymous");// for CORS policies
-        
-        //Create the figCaption element
-        const figcaption = document.createElement("figcaption");
-        figcaption.innerHTML = work.title;
-        
-        //append everything ( img and figaption -> figure -> gallery)
-        figure.appendChild(imageGallery);
-        figure.appendChild(figcaption);
-        myGalleryHTML.appendChild(figure); 
+        if (selectedCategory == 0 || work.categoryId == selectedCategory) {
+            const figure = document.createElement(`figure`);  
+            //Create the img element
+            const imageGallery = document.createElement(`img`);
+            imageGallery.setAttribute(`alt`, work.title);
+            imageGallery.setAttribute(`src`, work.imageUrl);
+            imageGallery.setAttribute(`crossOrigin`, `Anonymous`);// for CORS policies
+            //Create the figCaption element
+            const figcaption = document.createElement(`figcaption`);
+            figcaption.innerHTML = work.title;            
+            //append everything ( img and figaption -> figure -> gallery)
+            figure.appendChild(imageGallery);
+            figure.appendChild(figcaption);
+            myGalleryHTML.appendChild(figure); 
+        }
     }
 }
 
 
 async function getAllCategories() {
-    const categories = await fetch("http://localhost:5678/api/categories").then(d => d.json());
+    const categories = await fetch(`http://localhost:5678/api/categories`).then(d => d.json());
     return categories;
 }
-
+/**
+* 
+* @param {Backend categories list } categoriesArray 
+*/
 function filtersButton(categoriesArray){  
-    const filtersHTML = document.getElementById("filtersButton");
+    const filtersHTML = document.getElementById(`filtersButton`);
     
-    creatAndAppendButton(`Tous`,filtersHTML);    
-    for (const categorie of categoriesArray) {
-        console.log(categorie);        
-        creatAndAppendButton(categorie.name, filtersHTML);
+    creatAndAppendButton(`Tous`,0, filtersHTML);    
+    for (const category of categoriesArray) {                
+        creatAndAppendButton(category.name,category.id, filtersHTML);
     }
 }
 /**
 * Funtion use for creat a button.
-* @param {Display text on button} labelButton 
+* @param {Display text on button} buttonLabel 
+* @param {the id of category} categoryId
 * @param {Where to put button on page} locationToAppend 
 * <span id="filtersButton">
 * </span>
 */
-function creatAndAppendButton(labelButton, locationToAppend) {
-    const button = document.createElement("button");
+function creatAndAppendButton(buttonLabel, categoryId, locationToAppend) {
+    const button = document.createElement(`button`);
     
-    // Creat a class for labelButton to use it in CSS
-    button.setAttribute(`class`, `categorieButton`);
-
-    const nameButton = document.createTextNode(labelButton);
-    button.appendChild(nameButton);
-    locationToAppend.appendChild(button);
+    // Creat a class for labelButton to use it in CSS    
+    button.setAttribute(`class`, `buttonCategory`);
+    button.setAttribute(`data-id`, categoryId); 
+    
+    const buttonName = document.createTextNode(buttonLabel);
+    button.appendChild(buttonName);
+    locationToAppend.appendChild(button);  
+    
+    button.addEventListener(`click`, filterWorksCallback);
 }
+/**
+ * 
+ * @param {The event bubbled up by JavaScript for a click} event 
+ */
+function filterWorksCallback(event) {
+    const target = event.target;
+    const selectedCategory = parseInt(target.getAttribute(`data-id`));
+    constructWorksItems(JSON.parse(localStorage.getItem(`worksArray`)), selectedCategory);
+    
+}
+
+
+
 
